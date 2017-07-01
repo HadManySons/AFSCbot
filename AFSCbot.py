d1790849e104120b52f069b3d194a6dbb236c2fd
Andrew Pelletier <pelletierra@gmail.com>
2017-07-01 00:31:40 -0700

    refactored into functions and added exit for empty AFSCdict

--- AFSCbot.py
+++ AFSCbot.py
@@ -1,181 +1,207 @@
-import praw
-import sqlite3
 from pathlib import Path
-import random
 import logging
 import time
 import os
 import sys
 import csv
-# Initialize a logging object and have some examples below from the Python
-# Doc page
-logging.basicConfig(filename='AFSCbot.log', level=logging.INFO)
+import praw
+import sqlite3
 
-# Get the PID of this process
-pid = str(os.getpid())
-pidfile = "AFSCbot.pid"
+PID_FILE = "AFSCbot.pid"
+CRED_FILE = 'AFSCbotCreds.txt'
+DB_FILE = "AFSCbotCommentRecord.db"
+AFSC_FILE = 'AFSClist.csv'
+LOG_TIME_FORMAT = "%Y/%m/%d %H:%M:%S "
 
-# Exit if a version of the script is already running
-if os.path.isfile(pidfile):
-    print(pidfile + " already running, exiting")
-    sys.exit()
+# 'AFSCbot' must be changed to 'airforce' for a production version of the script
+#SUBREDDIT = 'airforce+airnationalguard'
+SUBREDDIT = 'AFSCbot'
 
-# Create the lock file for the script
-open(pidfile, 'w').write(pid)
 
-logging.info(time.strftime("%Y/%m/%d %H:%M:%S ") + "Starting script")
+def main():
 
-# reddit user object
-try:
-    creds = open('AFSCbotCreds.txt', 'r')
-    print("Opened creds file")
-    logging.info(time.strftime("%Y/%m/%d %H:%M:%S ") + "Opened creds file")
-except OSError:
-    print("Couldn't open ExcusesCreds.txt")
-    logging.error(time.strftime("%Y/%m/%d %H:%M:%S ") + "Couldn't open AFSCbotCreds.txt")
-    exit()
+    # Initialize a logging object and have some examples below from the Python Doc page
+    logging.basicConfig(filename='AFSCbot.log', level=logging.INFO)
+    logging.info(time.strftime(LOG_TIME_FORMAT) + "Starting script")
 
-credsUserAgent = creds.readline()
-credsClientID = creds.readline()
-credsClientSecret = creds.readline()
-credsUsername = creds.readline()
-credsPassword = creds.readline()
-creds.close()
+    # setup pid, database, and AFSC dict
+    open_pid()
+    conn, dbCommentRecord = setup_database()
+    AFSCdict = get_AFSCs()
 
-# Try to login or sleep/wait until logged in, or exit if user/pass wrong
-NotLoggedIn = True
-while NotLoggedIn:
+    # reddit user object
+    reddit = login()
+
+    # subreddit instance of /r/AirForce.
+    rAirForce = reddit.subreddit(SUBREDDIT)
+
+    process_comments(rAirForce, conn, dbCommentRecord, AFSCdict)
+
+
+def open_pid():
+    # Get the PID of this process
+    # Exit if a version of the script is already running
+    if os.path.isfile(PID_FILE):
+        print("pid already open, exiting script")
+        sys.exit(1)
+    else:
+        # Create the lock file for the script
+        pid = str(os.getpid())
+        open(PID_FILE, 'w').write(pid)
+
+
+def login():
     try:
-        reddit = praw.Reddit(
-            user_agent=credsUserAgent.strip(),
-            client_id=credsClientID.strip(),
-            client_secret=credsClientSecret.strip(),
-            username=credsUsername.strip(),
-            password=credsPassword.strip())
-        print("Logged in")
-        NotLoggedIn = False
-    except praw.errors.InvalidUserPass:
-        print("Wrong username or password")
-        logging.error(time.strftime("%Y/%m/%d %H:%M:%S ") + "Wrong username or password")
-        exit(1)
-    except Exception as err:
-        print(str(err))
-        time.sleep(5)
+        creds = open(CRED_FILE, 'r')
+        print("Opened creds file")
+        logging.info(time.strftime(LOG_TIME_FORMAT) + "Opened creds file")
+    except OSError:
+        print("Couldn't open {}".format(CRED_FILE))
+        logging.error(time.strftime(LOG_TIME_FORMAT) + "Couldn't open {}".format(CRED_FILE))
+        sys.exit(1)
 
-# vars
-globalCount = 0
-dbFile = Path("AFSCbotCommentRecord.db")
+    agent = creds.readline().strip()
+    ID = creds.readline().strip()
+    secret = creds.readline().strip()
+    client_user = creds.readline().strip()
+    client_password = creds.readline().strip()
+    creds.close()
 
-# check to see if database file exists
-if dbFile.is_file():
-    # connection to database file
-    conn = sqlite3.connect("AFSCbotCommentRecord.db")
-    # database cursor object
-    dbCommentRecord = conn.cursor()
-else:  # if it doesn't, create it
-    conn = sqlite3.connect("AFSCbotCommentRecord.db")
-    dbCommentRecord = conn.cursor()
-    dbCommentRecord.execute('''CREATE TABLE comments(comment text)''')
+    # Try to login or sleep/wait until logged in, or exit if user/pass wrong
+    NotLoggedIn = True
+    while NotLoggedIn:
+        try:
+            reddit = praw.Reddit(
+                user_agent=agent,
+                client_id=ID,
+                client_secret=secret,
+                username=client_user,
+                password=client_password)
+            print("Logged in")
+            NotLoggedIn = False
+        except praw.errors.InvalidUserPass:
+            print("Wrong username or password")
+            logging.error(time.strftime(LOG_TIME_FORMAT) + "Wrong username or password")
+            exit(1)
+        except Exception as err:
+            print(str(err))
+            time.sleep(5)
+    return reddit
 
-# subreddit instance of /r/AirForce. 'AFSCbot' must be changed to 'airforce' for a production version of the
-# script.
-#subreddit = 'airforce+airnationalguard'
-subreddit = 'AFSCbot'
-rAirForce = reddit.subreddit(subreddit)
 
-logging.info(time.strftime("%Y/%m/%d %H:%M:%S ") +
-             "Starting processing loop for subreddit: " + subreddit)
+def setup_database():
+    dbFile = Path(DB_FILE)
 
-AFSCdict = {}
-#AFSClist =  []
+    # check to see if database file exists
+    if dbFile.is_file():
+        # connection to database file
+        conn = sqlite3.connect(DB_FILE)
+        # database cursor object
+        dbCommentRecord = conn.cursor()
 
-#Load the AFSCs into a dictionary, then use the AFSC as the key to the Job Title
-with open('AFSClist.csv', newline='') as f:
-    reader = csv.reader(f, delimiter='#')
-    for row in reader:
-        afsc, jt = row
-        AFSCdict[afsc] = jt
+    else:  # if it doesn't, create it
+        conn = sqlite3.connect(DB_FILE)
+        dbCommentRecord = conn.cursor()
+        dbCommentRecord.execute('''CREATE TABLE comments(comment text)''')
+    return conn, dbCommentRecord
 
-#for keys in AFSCdict.keys():
-#    AFSClist.append(keys)
 
-while True:
-    try:
-        # stream all comments from /r/AirForce
-        for rAirForceComments in rAirForce.stream.comments():
-            globalCount += 1
-            print("\nComments processed since start of script: " + str(globalCount))
-            print("Processing comment: " + rAirForceComments.id)
+def get_AFSCs():
+    # Load the AFSCs into a dictionary, then use the AFSC as the key to the Job Title
+    AFSCdict = {}
+    with open(AFSC_FILE, newline='') as f:
+        reader = csv.reader(f, delimiter='#')
+        for row in reader:
+            afsc, jt = row
+            AFSCdict[afsc] = jt
 
-            # prints a link to the comment. A True for permalink generates a fast find (but is not an accurate link,
-            # just makes the script faster (SIGNIFICANTLY FASTER)
-            permlink = "http://www.reddit.com" + \
-                       rAirForceComments.permalink(True) + "/"
-            print(permlink)
-            logging.info(time.strftime("%Y/%m/%d %H:%M:%S ") +
-                         "Processing comment: " + permlink)
+    if AFSCdict == {}:
+        print("AFSC dict empty: exiting program")
+        sys.exit(1)
 
-            # Pulls all comments previously commented on
-            dbCommentRecord.execute(
-                "SELECT * FROM comments WHERE comment=?", (rAirForceComments.id,))
+    return AFSCdict
 
-            id_exists = dbCommentRecord.fetchone()
 
-            # Make sure we don't reply to the same comment twice or to the bot
-            # itself
-            if id_exists:
-                print("Already processed comment: " +
-                      str(rAirForceComments.id) + ", skipping")
-                continue
-            elif rAirForceComments.author == "AFSCbot":
-                print("Author was the bot, skipping...")
-                continue
-            else:
-                formattedComment = rAirForceComments.body
-                formattedComment = formattedComment.upper()
+def process_comments(rAirForce, conn, dbCommentRecord, AFSCdict):
+    logging.info(time.strftime(LOG_TIME_FORMAT) + "Starting processing loop for subreddit: " + SUBREDDIT)
+    comments_seen = 0
+    while True:
+        try:
+            # stream all comments from /r/AirForce
+            for rAirForceComments in rAirForce.stream.comments():
+                comments_seen += 1
+                print("\nComments processed since start of script: " + str(comments_seen))
+                print("Processing comment: " + rAirForceComments.id)
 
-                commentList = ""
-                matchList = []
-                for AFSC in AFSCdict.keys():
-                    if AFSC in formattedComment:
-                        matchList.append(AFSC)
-                        commentList += AFSC + " = " + AFSCdict[AFSC] + "\n\n"
+                # prints a link to the comment. A True for permalink generates a fast find (but is not an accurate link,
+                # just makes the script faster (SIGNIFICANTLY FASTER)
+                permlink = "http://www.reddit.com" + \
+                           rAirForceComments.permalink(True) + "/"
+                print(permlink)
+                logging.info(time.strftime(LOG_TIME_FORMAT) +
+                             "Processing comment: " + permlink)
+
+                # Pulls all comments previously commented on
+                dbCommentRecord.execute(
+                    "SELECT * FROM comments WHERE comment=?", (rAirForceComments.id,))
+
+                id_exists = dbCommentRecord.fetchone()
+
+                # Make sure we don't reply to the same comment twice or to the bot
+                # itself
+                if id_exists:
+                    print("Already processed comment: " +
+                          str(rAirForceComments.id) + ", skipping")
                     continue
-                if commentList != "":
-                    print("Commenting on AFSC: " + str(matchList) + " by: " + str(rAirForceComments.author)
-                          + ". Comment ID: " + rAirForceComments.id)
-                    logging.info(time.strftime("%Y/%m/%d %H:%M:%S ") +
-                                 "Commenting on AFSC: " + str(matchList) + " by: " + str(rAirForceComments.author) + ". Comment ID: " +
-                                 rAirForceComments.id)
-                    CommentReply = '^^You\'ve ^^mentioned ^^an ^^AFSC, ^^here\'s ^^the ^^associated ^^job ^^title:\n\n' \
-                                   + commentList
+                elif rAirForceComments.author == "AFSCbot":
+                    print("Author was the bot, skipping...")
+                    continue
+                else:
+                    formattedComment = rAirForceComments.body.upper()
 
-                    rAirForceComments.reply(CommentReply)
-                    dbCommentRecord.execute(
-                    'INSERT INTO comments VALUES (?);', (rAirForceComments.id,))
-                    conn.commit()
+                    commentList = ""
+                    matchList = []
+                    for AFSC in AFSCdict.keys():
+                        if AFSC in formattedComment:
+                            matchList.append(AFSC)
+                            commentList += AFSC + " = " + AFSCdict[AFSC] + "\n\n"
+                        continue
+                    if commentList != "":
+                        print("Commenting on AFSC: " + str(matchList) + " by: " + str(rAirForceComments.author)
+                              + ". Comment ID: " + rAirForceComments.id)
+                        logging.info(time.strftime(LOG_TIME_FORMAT) +
+                                     "Commenting on AFSC: " + str(matchList) + " by: " + str(rAirForceComments.author) + ". Comment ID: " +
+                                     rAirForceComments.id)
+                        CommentReply = '^^You\'ve ^^mentioned ^^an ^^AFSC, ^^here\'s ^^the ^^associated ^^job ^^title:\n\n' \
+                                       + commentList
 
-    # what to do if Ctrl-C is pressed while script is running
-    except KeyboardInterrupt:
-        print("Keyboard Interrupt experienced, cleaning up and exiting")
-        conn.commit()
-        conn.close()
-        print("Exiting due to keyboard interrupt")
-        logging.info(time.strftime("%Y/%m/%d %H:%M:%S ")
-                     + "Exiting due to keyboard interrupt")
-        exit(0)
+                        rAirForceComments.reply(CommentReply)
+                        dbCommentRecord.execute(
+                        'INSERT INTO comments VALUES (?);', (rAirForceComments.id,))
+                        conn.commit()
 
-    except KeyError:
-        print("AFSC not found in the dictionary, but the dict was called for some reason")
-        logging.error(time.strftime("%Y/%m/%d %H:%M:%S ")
-                      + "AFSC not found in the dictionary, but the dict was called for some reason")
+        # what to do if Ctrl-C is pressed while script is running
+        except KeyboardInterrupt:
+            print("Keyboard Interrupt experienced, cleaning up and exiting")
+            conn.commit()
+            conn.close()
+            print("Exiting due to keyboard interrupt")
+            logging.info(time.strftime(LOG_TIME_FORMAT)
+                         + "Exiting due to keyboard interrupt")
+            os.unlink(PID_FILE)
+            exit(0)
+
+        except KeyError:
+            print("AFSC not found in the dictionary, but the dict was called for some reason")
+            logging.error(time.strftime(LOG_TIME_FORMAT)
+                          + "AFSC not found in the dictionary, but the dict was called for some reason")
+
+        except Exception as err:
+            print("Exception: " + str(err))
+            logging.error(time.strftime(LOG_TIME_FORMAT)
+                          + "Unhandled exception: " + str(err))
+    #os.unlink(PID_FILE)
 
 
-    except Exception as err:
-        print("Exception: " + str(err))
-        logging.error(time.strftime("%Y/%m/%d %H:%M:%S ")
-                      + "Unhandled exception: " + str(err))
-
-
-    finally:
-        os.unlink(pidfile)
+if __name__ == "__main__":
+    main()
